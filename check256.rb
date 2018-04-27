aba8d1791583e72315c4ad9e152903f4b7563ef0
Cj-bc <cj-bc@y-modify.org>
2018-04-27 18:27:35 +0900

    Add: sha256 sum for v0.1.1

--- check256.rb
+++ check256.rb
@@ -3,6 +3,7 @@ class Check256 < Formula
   homepage "https://github.com/Cj-bc/check256.git"
   head "https://github.com/Cj-bc/check256.git"
   url "https://github.com/Cj-bc/check256/archive/v0.1.1.tar.gz"
+  sha256 "b0bc196dd0975b62745ab4e5778e93b426cdb548de6a8484bea100b2323bd910"
 
   def install
     system "mv", "check256.sh", "check256"
