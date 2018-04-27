f7ef9e576646a8de1b27f7fb5563cbe1d33462f1
Cj-bc <cj-bc@y-modify.org>
2018-04-27 18:23:51 +0900

    Fix: remove unwanted letter

--- check256.rb
+++ check256.rb
@@ -2,7 +2,7 @@ class Check256 < Formula
   desc "calculate sha256 hash of given version of given repository on github for homebrew"
   homepage "https://github.com/Cj-bc/check256.git"
   head "https://github.com/Cj-bc/check256.git"
-  url "https://github.com/Cj-bc/check256˜/archive/v0.1.1.tar.gz"
+  url "https://github.com/Cj-bc/check256/archive/v0.1.1.tar.gz"
 
   def install
     system "mv", "check256.sh", "check256"
